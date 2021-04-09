e184f6d12c2f183cc8f3af4af300b795fd0b6c5f
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-04-09 22:35:03 +0800

    new implementation for redirect

---
+++ pages/analytics.tsx
@@ -0,0 +1,8 @@
+import { useEffect } from "react"
+
+export default () => {
+  useEffect(() => {
+    window.location.assign(`https://www.hockeystack.com/sharing/07582b6b2e6b5a19f2ffdc0489c79b/dashboard`)
+  }, [])
+  return null
+}
