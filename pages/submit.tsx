ffd8a52e0565cdfe92b2e2294c7d31bba156cb13
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-04-10 08:35:54 +0800

    add form redirection

---
+++ pages/submit.tsx
@@ -0,0 +1,11 @@
+
+export default () => null
+
+export const getServerSideProps = async context => {
+	return {
+		redirect: {
+			destination: `https://docs.google.com/forms/d/1qc29SafinC09L0wrPLDeOab_ouUThEa-gipSySFeYC0/edit`,
+			permanent: false,
+		},
+	}
+}
